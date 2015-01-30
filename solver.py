f09fb7d73adc78e1f990383157c1d6a9d809eefb
champinthere <shreyasb99@gmail.com>
2015-01-30 14:30:48 -0600

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,2 +1,9 @@
 import numpy as np
 import scipy as sp
+
+def step2(p0, samp_size, context):
+  text = "Conditions\n\n"
+  text += "\\begin{enumerate}\n"
+  text += "\\item Random Sample (Stated)\n"
+  text += "\\item %d < 10 percent of %s\n" % (samp_size, context)
+  text += "\\item \(np_0 = "
